f49a16c563db77cf20a77b55a094ac1d2bd24a4e
Volodymyr M. Lisivka <vlisivka@gmail.com>
2026-01-08 11:06:32 +0200

    3 more levels are added to database.

--- createDB.bash
+++ createDB.bash
@@ -9,11 +9,30 @@ mkdir -p /var/www-data
 
 sqlite3 /var/www-data/netsim.sqlite3 <<'END_SQL'
 CREATE TABLE user (id integer PRIMARY KEY,name text,password text);
-INSERT INTO user (name, password) VALUES ('netsim','$2y$10$P1HF8dAfF55rzE93LAunaOnMoePALZxUnbJSl0MSxajAdbyB6SbqG');
 CREATE TABLE category (id integer PRIMARY KEY,name text,orderby integer);
-INSERT INTO category (name, orderby) VALUES('Basics', 1),('Spoofs', 2),('Denial of Service', 3),('Attacks', 4);
+INSERT INTO category (name, orderby) VALUES
+  ('Basics', 1),
+  ('Spoofs', 2),
+  ('Denial of Service', 3),
+  ('Attacks', 4),
+  ('Routing', 5);
 CREATE TABLE level (id integer PRIMARY KEY,category_id integer,name text,orderby integer,filename text);
-INSERT INTO level (category_id, name, orderby, filename) VALUES(1, 'Getting started', 1, '01 Basics/level01'),(1, 'Packet fields', 2, '01 Basics/level02'),(1, 'Ping', 3, '01 Basics/level03'),(1, 'Routing', 4, '01 Basics/level04'),(1, 'Modems', 5, '01 Basics/level05'),(2, 'IP Spoofing', 1, '02 Spoofs/spoofs01'),(2, 'Stealing packets', 2, '02 Spoofs/spoofs02'),(3, 'Basic DoS', 1, '03 DoS/dos01'),(3, 'Distributed DoS', 2, '03 DoS/dos02'),(3, 'Smurf attack', 3, '03 DoS/dos03'),(4, 'Man-in-the-middle', 1, '04 Attacks/attacks01'),(4, 'Censorship', 2, '04 Attacks/attacks02'), (4, 'Traceroute', 3, '04 Attacks/attacks03');
+INSERT INTO level (category_id, name, orderby, filename) VALUES
+  (1, 'Getting started', 1, '01 Basics/level01'),
+  (1, 'Packet fields', 2, '01 Basics/level02'),
+  (1, 'Ping', 3, '01 Basics/level03'),
+  (1, 'Routing', 4, '01 Basics/level04'),
+  (1, 'Modems', 5, '01 Basics/level05'),
+  (2, 'IP Spoofing', 1, '02 Spoofs/spoofs01'),
+  (2, 'Stealing packets', 2, '02 Spoofs/spoofs02'),
+  (3, 'Basic DoS', 1, '03 DoS/dos01'),
+  (3, 'Distributed DoS', 2, '03 DoS/dos02'),
+  (3, 'Smurf attack', 3, '03 DoS/dos03'),
+  (4, 'Man-in-the-middle', 1, '04 Attacks/attacks01'),
+  (4, 'Censorship', 2, '04 Attacks/attacks02'),
+  (4, 'Traceroute', 3, '04 Attacks/attacks03'),
+  (5, 'Static Routing', 1, '05 Routing/static01'),
+  (5, 'Dynamic Routing', 2, '05 Routing/distancevector01');
 CREATE TABLE solns (id integer PRIMARY KEY,user_id integer,level_id integer,completed integer,json text);
 END_SQL
 

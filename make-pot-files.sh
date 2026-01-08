9755807e01de778213d8f771aa2e3bff6cbf5bdb
Volodymyr M. Lisivka <vlisivka@gmail.com>
2026-01-08 11:14:51 +0200

    Fix: path to directory with .pot files updated in script.

--- make-pot-files.sh
+++ make-pot-files.sh
@@ -6,5 +6,5 @@ TRANSLATIONS_DIR="./translations"
 for FILE in levels/*/*.html
 do
   FILE_NAME=$(basename "$FILE")
-  html2po --pot -i "$FILE" -o "$TRANSLATIONS_DIR/${FILE_NAME%.html}.pot"
+  html2po --pot -i "$FILE" -o "$TRANSLATIONS_DIR/pot/${FILE_NAME%.html}.pot"
 done
